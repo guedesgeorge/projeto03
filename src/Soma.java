3713f2e42448deb763c0834f624dd573316643e6
´´George <george.emannuelcarvalho@gmail.com>
2024-05-26 16:04:56 -0400

    Merge remote-tracking branch 'github/master'

--- src/Soma.java
+++ src/Soma.java
@@ -1,6 +1,6 @@
 public class Soma {
     public static void main(String[] args) {
-        int resultado = 100 / 98;
+        int resultado = 100 + 98;
         System.out.println(resultado);
     }
 }
